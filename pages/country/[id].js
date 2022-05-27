106fd5067429b056d12792916209488dbddf7554
Ravi Rafaliya <ravirafaliya11@gmail.com>
2022-05-27 08:55:20 -0400

    api error fixed

--- pages/country/[id].js
+++ pages/country/[id].js
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 const getCountry = async (id) => {
-  const res = await fetch(`https://restcountries.com/rest/v2/alpha/${id}`);
+  const res = await fetch(`https://restcountries.com/v2/alpha/${id}`);
 
   const country = await res.json();
 
@@ -131,7 +131,7 @@ const Country = ({ country }) => {
 export default Country;
 
 export const getStaticPaths = async () => {
-  const res = await fetch("https://restcountries.com/rest/v2/all");
+  const res = await fetch("https://restcountries.com/v2/all");
   const countries = await res.json();
 
   const paths = countries.map((country) => ({
